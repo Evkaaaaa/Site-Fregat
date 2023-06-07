e48ed7afa16189468957bad5f921036b0cedfc95
Evkaaaaa <119289626+Evkaaaaa@users.noreply.github.com>
2023-06-07 11:37:08 +0300

    Delete link-img.js

--- link-img.js
+++
@@ -1,6 +0,0 @@
-function showLinkImage(id) {
-    document.getElementById(id).style.display = "block";
-  }
-  function hideLinkImage(id) {
-    document.getElementById(id).style.display = "none";
-  }
